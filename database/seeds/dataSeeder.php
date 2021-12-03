e042452fb9dd2a9c7f92f96e9d60834dbe71c400
pakawa123@gmail.com <pakawa123@gmail.com>
2021-12-03 20:29:56 +0700

    add seeder

--- database/seeds/dataSeeder.php
+++ database/seeds/dataSeeder.php
@@ -1,24 +1,47 @@
 <?php
 
-use Illuminate\Database\Seeder;
+use Illuminate\ Database\ Seeder;
 
-class dataSeeder extends Seeder
-{
+class dataSeeder extends Seeder {
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
+    public
+    function run() {
         //
         DB::table('data')->insert([
-                [
-                'name' => 1,
-                'build' => 1,
-                'depart' => 1,
-                'tel' => 1,
-                ]
+            [
+                'name' => 'Syeda',
+                'build' => 'A',
+                'depart' => 'Doctor',
+                'tel' => '6622582664',
+            ],
+            [
+                'name' => 'Fern',
+                'build' => 'A',
+                'depart' => 'Doctor',
+                'tel' => '0294853114',
+            ],
+            [
+                'name' => 'Gabriella',
+                'build' => 'A',
+                'depart' => 'Doctor',
+                'tel' => '0819459162',
+            ],
+            [
+                'name' => 'Khadija',
+                'build' => 'B',
+                'depart' => 'Marketing',
+                'tel' => '6625863213',
+            ],
+            [
+                'name' => 'Khadija',
+                'build' => 'B',
+                'depart' => 'Marketing',
+                'tel' => '6624511433',
+            ],
         ]);
     }
 }
